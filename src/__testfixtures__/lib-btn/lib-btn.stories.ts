b267ebfb51e6bf276b4204d323f8e8126ff55a51
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2026-06-29 00:46:34 +0200

    chore: update Storybook imports to use angular-vite for button and lib-btn stories

--- src/__testfixtures__/lib-btn/lib-btn.stories.ts
+++ src/__testfixtures__/lib-btn/lib-btn.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/angular";
+import type { Meta, StoryObj } from "@storybook/angular-vite";
 import { LibBtnDirective } from "./lib-btn.directive";
 
 const meta: Meta<LibBtnDirective> = {
